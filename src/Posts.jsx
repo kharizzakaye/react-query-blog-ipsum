9b52d33be26794cfe4815d63cdca2c1b985d6914
Khariza Viloria <kharizzakaye@gmail.com>
2025-03-19 17:22:02 +0800

    Add Pagination

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -6,12 +6,12 @@ import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   const { data,  isError, isLoading } = useQuery({
-    queryKey: ["posts"], // definition of data
-    queryFn: fetchPosts, // function that will run to get data
+    queryKey: ["posts", currentPage], // definition of data
+    queryFn: () => fetchPosts(currentPage), // function that will run to get data
     staleTime: 2000, // when to trigger a refetch - if not stated, staleTime = 0ms,
   });
   
@@ -43,11 +43,19 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button 
+          disabled={currentPage <= 1} 
+          onClick={() => { setCurrentPage((previousValue => previousValue - 1))}}
+        >
           Previous page
         </button>
+
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+
+        <button 
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {setCurrentPage((previousValue => previousValue + 1))}}
+        >
           Next page
         </button>
       </div>
